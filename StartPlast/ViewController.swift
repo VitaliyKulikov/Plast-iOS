a5d5ff5d1514050d33f8b546725540dfa1e8d527
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2019-04-06 20:20:41 +0300

    Added assets and updated pods.

--- StartPlast/ViewController.swift
+++ StartPlast/ViewController.swift
@@ -10,24 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         self.performSegue(withIdentifier: "LoginIdentifier", sender: self)
     }
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-    
-    
-
-
 }
 
--- StartPlast/ViewControllers/LoginViewController/LoginViewController.swift
+++ StartPlast/ViewControllers/LoginViewController/LoginViewController.swift
@@ -12,6 +12,8 @@ import GoogleSignIn
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
+    private static let kMainProgramId = "CarouselIdentifier"
+    
     @IBOutlet private weak var signInButton: GIDSignInButton!
     
     override func viewDidLoad() {
@@ -22,5 +24,26 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
 
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+            self.performSegue(withIdentifier: LoginViewController.kMainProgramId,
+                         sender:  ProfileModel(
+                            username: "Kristina Del Rio Albrechet",
+                            email: "kalbrechet@gmail.com",
+                            avatarUrl: URL(string: "https://cdn-images-1.medium.com/fit/c/200/200/1*8EAtAFUVRiK1btuFhLnE1Q@2x.jpeg")!,
+                            coins: 0))
+        }
+        
+    }
+}
 
+extension LoginViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == LoginViewController.kMainProgramId,
+            let controller = segue.destination as? MainProgramController,
+            let model = sender as? ProfileModel {
+            controller.profileModel = model
+        }
+    }
 }
