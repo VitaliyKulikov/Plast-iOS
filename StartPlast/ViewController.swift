3b07ad6331c43f6c0b60da4d73ab6cb0538a5e22
Viktor Rudyk <rudykvictor@gmail.com>
2019-04-06 18:17:08 +0300

    Added google auth

--- StartPlast/ViewController.swift
+++ StartPlast/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.performSegue(withIdentifier: "LoginIdentifier", sender: self)
+    }
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+    
+    
 
 
 }
